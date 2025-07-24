3ecd2cd6b8ee223e4b57682ad341d8510254282c
Himas1321 <nikita5045@mail.ru>
2025-07-24 21:06:50 +0900

    Update lesson 6.py

--- lesson 6.py
+++ lesson 6.py
@@ -1,28 +1,32 @@
 user_input = input('ведите пароль:')
-line_counting = len(user_input)
-# checking_numbers_or_letters = user_input.isdigit()
-# print('Длина пароля:', line_counting)
-if line_counting < 12:
-	print('короткий')
-else:
-	print('длинный')
 
-
-
-for letter in user_input:
-	user_input = letter.isdigit()
-	if user_input == False:
-		print(letter,'- Буква')
-	else:
-		print(letter,'- Цифра')
-
-
-def check_numbers_in_password(number):
-	humber_found = False
-	print('есть цифр')
-	for numbers in letter:
-		if number.number():
-			number_found = True
-			print('Нет цифр')
-	return number_found
+def get_long_or_short_password(password):
+	return len(password) >= 12
 	
+def get_whether_there_are_numbers_or_not(password):
+	return any(letter.isdigit() for letter in password)
+
+def has_upper_letters(password):
+	return any(letter.isupper() for letter in password)
+
+def has_lower_letters(password):
+	return any(letter.islower() for letter in password)
+
+def has_symbols(password):
+	return any(not letter.isalpha() and not letter.isdigit() for letter in password)
+	
+checks = [
+	get_long_or_short_password, get_whether_there_are_numbers_or_not, 
+	has_upper_letters, has_lower_letters, 
+	has_symbols
+]
+
+def password_rating(password):
+    score = 0
+    for check in checks:
+        if check(password):
+            score += 2
+    return(score)
+
+if __name__ == '__main__':
+	print('Рейтинг пароля:', password_rating(user_input))
