5146ce5b75b209de9184cf9b6d75ccd6e0cfde5b
Himas1321 <nikita5045@mail.ru>
2025-07-27 11:23:59 +0900

    Update lesson 6.py

--- lesson 6.py
+++ lesson 6.py
@@ -1,24 +1,25 @@
-user_input = input('ведите пароль:')
-
 def get_long_or_short_password(password):
-	return len(password) >= 12
-	
+    return len(password) >= 12
+
 def get_whether_there_are_numbers_or_not(password):
-	return any(letter.isdigit() for letter in password)
+    return any(letter.isdigit() for letter in password)
 
 def has_upper_letters(password):
-	return any(letter.isupper() for letter in password)
+    return any(letter.isupper() for letter in password)
 
 def has_lower_letters(password):
-	return any(letter.islower() for letter in password)
+    return any(letter.islower() for letter in password)
 
 def has_symbols(password):
-	return any(not letter.isalpha() and not letter.isdigit() for letter in password)
-	
+    return any(not letter.isalpha() and not letter.isdigit() for letter in
+               password)
+
 checks = [
-	get_long_or_short_password, get_whether_there_are_numbers_or_not, 
-	has_upper_letters, has_lower_letters, 
-	has_symbols
+    get_long_or_short_password,
+    get_whether_there_are_numbers_or_not,
+    has_upper_letters,
+    has_lower_letters,
+    has_symbols
 ]
 
 def password_rating(password):
@@ -26,7 +27,8 @@ def password_rating(password):
     for check in checks:
         if check(password):
             score += 2
-    return(score)
+    return (score)
 
 if __name__ == '__main__':
-	print('Рейтинг пароля:', password_rating(user_input))
+    user_input = input('ведите пароль:')
+    print('Рейтинг пароля:', password_rating(user_input))
